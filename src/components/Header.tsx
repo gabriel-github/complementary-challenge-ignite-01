d558245da09558af6d3f304216f883c3efaf3399
gabriel-github <gabrielferrsantos201@gmail.com>
2021-07-12 19:18:38 -0300

    solved challenge

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+interface HeaderProps {
+  selectedGenre: string;
+}
+
+export function Header({ selectedGenre }: HeaderProps) {
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {selectedGenre}</span>
+      </span>
+    </header>
+  );
+}
